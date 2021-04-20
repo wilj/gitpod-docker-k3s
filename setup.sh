bf7a4501a4fbdc00b21f49a81f07732b4c16f109
Matej Jelić <programming@bymatej.com>
2021-04-20 09:37:12 +0200

    Update ports for testing purposes

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -euox
 DOMAIN=$1
 DNSSERVER=$2
 
+# TEST:
 docker build . -t gitpod-k3s-local
 docker run \
     --name gitpod \
@@ -13,6 +14,21 @@ docker run \
     -e DOMAIN=$DOMAIN \
     -e DNSSERVER=$DNSSERVER \
     --privileged \
-    -p 80:80 \
-    -p 443:443 \
+    -p 81:80 \
+    -p 444:443 \
     gitpod-k3s-local
+    
+    
+## ORIGINAL:
+#docker build . -t gitpod-k3s-local
+#docker run \
+#    --name gitpod \
+#    -v /run/containerd/containerd.sock:/run/containerd/containerd.sock \
+#    -v /tmp/workspaces:/var/gitpod/workspaces \
+#    -v $(pwd)/certs:/certs \
+#    -e DOMAIN=$DOMAIN \
+#    -e DNSSERVER=$DNSSERVER \
+#    --privileged \
+#    -p 80:80 \
+#    -p 443:443 \
+#    gitpod-k3s-local
